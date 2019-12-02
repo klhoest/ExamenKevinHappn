03fae5fb90ae79379fc6bb2f5d9b881a605405ec
Kevin desktop <k.lhoest@groupeonepoint.com>
2019-12-02 20:42:28 +0100

    remove DayRepositoryStub and use mock instead

--- app/src/test/java/com/lhoest/kevin/happn/examenkevinhappn/ForecastViewModelTest.kt
+++ app/src/test/java/com/lhoest/kevin/happn/examenkevinhappn/ForecastViewModelTest.kt
@@ -1,8 +1,12 @@
 package com.lhoest.kevin.happn.examenkevinhappn
 
 import com.lhoest.kevin.happn.examenkevinhappn.model.Day
-import com.lhoest.kevin.happn.examenkevinhappn.repository.DayRepositoryStub
+import com.lhoest.kevin.happn.examenkevinhappn.model.Main
+import com.lhoest.kevin.happn.examenkevinhappn.model.Weather
+import com.lhoest.kevin.happn.examenkevinhappn.repository.DayRepository
 import com.lhoest.kevin.happn.examenkevinhappn.viewmodel.ForecastViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.Assert.*
@@ -11,7 +15,17 @@ class ForecastViewModelTest {
 
     @Test
     fun mapDayListToSummaryList() {
-        val repoStub = DayRepositoryStub()
+        val repoStub = mockk<DayRepository>()
+
+        val stubMain1 = Main(1.0, 2.0, 3.0, 1000.0, 500.0, 700.0, 3, 50.0)
+        val stubWeather = Weather(1, "cloud", "some clouds", null)
+        val itemList = listOf(
+                Day(1485799200, stubMain1, listOf(stubWeather), null, null, null, null, null)
+        )
+
+        //if we want to mock a list we must mock list.size and list.iterator
+        coEvery { repoStub.getDayList() } returns itemList
+
         val viewModel = ForecastViewModel(repoStub)
         runBlocking {
             val dayList: List<Day> = repoStub.getDayList()
